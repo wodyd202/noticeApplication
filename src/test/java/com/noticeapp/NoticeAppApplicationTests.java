e61f794e64ceda31a62aec076705899021cb3e70
wodyd202 <wodyd202@daum.net>
2022-01-08 21:21:55 +0900

    필요없는 테스트 제거

--- src/test/java/com/noticeapp/NoticeAppApplicationTests.java
+++ src/test/java/com/noticeapp/NoticeAppApplicationTests.java
@@ -1,15 +1,8 @@
 package com.noticeapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class NoticeAppApplicationTests {
-
-	@Test
-	void contextLoads() {}
 
 }
